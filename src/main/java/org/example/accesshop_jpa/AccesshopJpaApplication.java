d7dd38aeca0be542a1fab284a32fec87c372d6e1
Antonio Bernardi <aberben0907@g.educaand.es>
2025-03-05 14:01:42 +0100

    Proyecto a empezar

---
+++ src/main/java/org/example/accesshop_jpa/AccesshopJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.accesshop_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesshopJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesshopJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/accesshop_jpa/AccesshopJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.accesshop_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesshopJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
